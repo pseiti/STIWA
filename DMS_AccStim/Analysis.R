5708d6b3fe857be9f12a540917cd5fab1ea74840
pseiti <36700524+pseiti@users.noreply.github.com>
2025-07-09 22:32:29 +0200

    sdf

--- DMS_AccStim/Analysis.R
+++ DMS_AccStim/Analysis.R
@@ -1,34 +1,81 @@
 
-# path_to_data <- "~/Desktop/Schreibtisch_0/FFG-Haptics/Exp_AT_synchron_1/data/"
-# setwd(path_to_data)
-# allFileNames <- list.files(path=path_to_data)
-# dmsData_ids <- as.vector(sapply(allFileNames,function(i){grepl("dms",i)}))
-# dmsData_fileNames <- allFileNames[dmsData_ids]
-# N_sample <- length(dmsData_fileNames)
+path_to_data <- "~/Dokumente/GitHub/STIWA/DMS_AccStim/Data"
+setwd(path_to_data)
+allFileNames <- list.files(path=path_to_data)
+dmsData_ids <- as.vector(sapply(allFileNames,function(i){grepl("dms",i)}))
+dmsData_fileNames <- allFileNames[dmsData_ids]
+# merging data
+for (x in dmsData_fileNames) {
+  code_x_data <- read.csv(file = x, header = TRUE)
+  if(x==dmsData_fileNames[1]){data_merged <- code_x_data}else{
+    data_merged <- rbind(data_merged,code_x_data)
+  }
+}
+test_data <- data_merged[data_merged$practice=="False",]
+unique_codes <- unique(test_data$pcode)
+N_sample <- length(unique_codes)
 
-# # merging data
-# for (x in dmsData_fileNames) {
-#   code_x_data <- read.csv(file = x, header = TRUE)
-#   if(x==dmsData_fileNames[1]){data_merged <- code_x_data}else{
-#     data_merged <- rbind(data_merged,code_x_data)
-#   }
-# }
-# test_data <- data_merged[data_merged$practice=="False",]
+print(paste("Sample size: ",N_sample))
 
-# # aggregate over conditions per participant
-# for (file_i in dmsData_fileNames) {
-#   # file_i <- dmsData_fileNames[1]
-#   file_i_content <- read.csv(file_i, header = T)
-#   # head(file_i_content)
-#   # loop structure: 
-#   # # queried_target (T=1 and T!=1, or, T=2 and T=!2) 
-#   # # hits (e.g., a T1-trial with a 'T=1?'-question and a 'yes' response)
-#   # # fas (e.g., a T1-trial with a  'T=2'-question and  a 'yes' response)
-#   P_is_T1_rows <- file_i_content$question=="P == T1?"
-#   P_isNot_T1_rows <- file_i_content$question=="P != T1?"
+# aggregate over conditions per participant
+df_rocPoints_individual <- NULL
+for(code_i in c(1:N_sample)){
+  curCode <- unique_codes[code_i]
+  curCodeData <- test_data[test_data$pcode==curCode,]
+  Same_trials <- curCodeData$PTS=="Same"
+  curCode_sameData <- curCodeData[Same_trials,]
+  T1_trials <- curCode_sameData$TargetPosition==1
+  qType_equal_trials <- curCode_sameData$questionType=="equal"
+  respYes_trials <- curCode_sameData$response_dicho=="Yes"
   
-    
-# }
+  hitRates_123_data <- curCode_sameData[T1_trials&qType_equal_trials&respYes_trials,]
+  fqs_hitRates_123 <- table(hitRates_123_data$response_rating)
+  fq_ids <- match(c("sure","quiteSure","unsure"),names(fqs_hitRates_123))
+  fqs_hitRates_123 <- fqs_hitRates_123[fq_ids]
+  
+  hitRates_456_data <- curCode_sameData[T1_trials&(qType_equal_trials==FALSE)&respYes_trials,]
+  fqs_hitRates_456 <- table(hitRates_456_data$response_rating)
+  fq_ids <- match(c("unsure","quiteSure","sure"),names(fqs_hitRates_456))
+  fqs_hitRates_456 <- fqs_hitRates_456[fq_ids]
+  
+  fqs_hits <- c(fqs_hitRates_123,fqs_hitRates_456)
+  props_hits <- fqs_hits / sum(fqs_hits)
+  hitRates_cumul <- cumsum(props_hits)
 
-# print(P_is_T1_rows[:4])
-print("hello")
+  faRates_123_data <- curCode_sameData[(T1_trials==FALSE)&qType_equal_trials&respYes_trials,]
+  fqs_faRates_123 <- table(faRates_123_data$response_rating)
+  fq_ids <- match(c("sure","quiteSure","unsure"),names(fqs_faRates_123))
+  fqs_faRates_123 <- fqs_faRates_123[fq_ids]
+  
+  faRates_456_data <- curCode_sameData[(T1_trials==FALSE)&(qType_equal_trials==FALSE)&respYes_trials,]
+  fqs_faRates_456 <- table(faRates_123_data$response_rating)
+  fq_ids <- match(c("unsure","quiteSure","sure"),names(fqs_faRates_456))
+  fqs_faRates_456 <- fqs_faRates_456[fq_ids]
+  
+  fqs_fas <- c(fqs_faRates_123,fqs_faRates_456)
+  props_fas <- fqs_fas / sum(fqs_fas)
+  faRates_cumul <- cumsum(props_fas)
+
+  curCode_df <- data.frame(curCode)
+  for (i in 1:10) {
+    if(i<6) {
+      curCode_df <- cbind(curCode_df, hitRates_cumul[i])
+      } else { 
+      curCode_df <- cbind(curCode_df, faRates_cumul[i-5])
+    }
+  }
+  colnames(curCode_df) <- c("code",paste("hitRate_",1:5,sep=""),paste("faRate_",6:10,sep=""))
+  if(code_i==1){
+    df_rocPoints_individual <- curCode_df
+  } else {
+    df_rocPoints_individual <- rbind(df_rocPoints_individual, curCode_df)
+  }
+}
+roc_means <- colMeans(df_rocPoints_individual[,2:11],na.rm=T)
+
+path_to_plot <- "~/Dokumente/GitHub/STIWA/DMS_AccStim/"
+setwd(path_to_data)
+pdf("test.pdf")
+plot(roc_means[1:5]~roc_means[6:10],xlim=c(0,1),ylim=c(0,1))
+points(c(0,1),c(0,1),type="l")
+dev.off()
---
+++ DMS_AccStim/untitled.R
@@ -0,0 +1,8 @@
+df <- data.frame()
+df[1,paste("r_",1)] <- 1
+print(df)
+
+df = data.frame()
+# df = rbind(df,1)
+df = cbind(df,2)
+print(df)
