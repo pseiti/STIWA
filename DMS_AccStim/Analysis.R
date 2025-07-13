4c5594e1ba0d7074021120b3e5abe5556892a08c
pseiti <36700524+pseiti@users.noreply.github.com>
2025-07-13 12:35:24 +0200

    Update Analysis.R

--- DMS_AccStim/Analysis.R
+++ DMS_AccStim/Analysis.R
@@ -27,14 +27,13 @@ for (i in 1:nrow(test_data)) {
   queriedPosition_i <- i_question_split[length(i_question_split)-1]
   test_data[i,"queriedPosition"] <- queriedPosition_i
 }
-print(head(test_data))
 
 print(paste("Sample size: ",N_sample))
 
 # compute and insert variable: queriedItem
 # .....
 
-aggrFx <- function(pts,tns,qPos,accPos){
+aggrFx <- function(pts,tns,accPos){
   
   df_rocPoints_individual <- NULL
   qTypeNames <- c("P = T1?","P = T2?")
@@ -45,21 +44,23 @@ aggrFx <- function(pts,tns,qPos,accPos){
 
     pts_x_trials <- curCodeData$PTS==pts
     tns_x_trials <- curCodeData$TNS==tns
-    queriedPosition_x_trials <- curCodeData$queriedPosition==qPos
+    # queriedPosition_x_trials <- curCodeData$queriedPosition==qPos
     accessoryPosition_x_trials <- curCodeData$AccessoryPosition==accPos
 
-    curCode_condiData <- curCodeData[pts_x_trials&tns_x_trials&queriedPosition_x_trials&accessoryPosition_x_trials,]
-
+    # curCode_condiData <- curCodeData[pts_x_trials&tns_x_trials&queriedPosition_x_trials&accessoryPosition_x_trials,]
+    # curCode_condiData_eqQ1 <- curCode_condiData[curCode_condiData$question=="P = T1?",]
+    # curCode_condiData_xqQ1 <- curCode_condiData[curCode_condiData$question=="P != T1?",]
+    # curCode_condiData_eqQ2 <- curCode_condiData[curCode_condiData$question=="P = T2?",]
+    # curCode_condiData_xqQ2 <- curCode_condiData[curCode_condiData$question=="P != T2?",]
+    curCode_condiData <- curCodeData[pts_x_trials&tns_x_trials&accessoryPosition_x_trials,]
     curCode_condiData_eqQ1 <- curCode_condiData[curCode_condiData$question=="P = T1?",]
-    curCode_condiData_xqQ1 <- curCode_condiData[curCode_condiData$question=="P != T1?",]
     curCode_condiData_eqQ2 <- curCode_condiData[curCode_condiData$question=="P = T2?",]
-    curCode_condiData_xqQ1 <- curCode_condiData[curCode_condiData$question=="P != T2?",]
-
     list_qType_specific_data <- list(PisT1=curCode_condiData_eqQ1,PisT2=curCode_condiData_eqQ2)
 
     df_code_i <- NULL; df2_code_i <- NULL
     for (x in 1:2) {
       curData <- list_qType_specific_data[[x]]
+      # if(x==2){print(head(curData))}
       respYes_trials <- curData$response_dicho=="Yes"
       T1_trials <- curData$TargetPosition==1
       if(x==1) {
@@ -84,45 +85,51 @@ aggrFx <- function(pts,tns,qPos,accPos){
         return(fqs_xRate_perConfidence)
       }
       fqs_hitRate_perConfidence <- fx(data_x=data_hitRate)
-      fqs_hitRate_sum <- sum(fqs_hitRate_perConfidence,na.rm=T)
-      df_code_i <- rbind(df_code_i,
-        data.frame(code=curCode,qType=qTypeNames[x],targetPosition=1,
-          rating=c("sure","quiteSure","unsure"),
-          respType="hit",fq=fqs_hitRate_perConfidence)
-        )
+      fqs_hitRate_sum <- sum(fqs_hitRate_perConfidence)
+      # df_code_i <- rbind(df_code_i,
+      #   data.frame(code=curCode,qType=qTypeNames[x],targetPosition=1,
+      #     rating=c("sure","quiteSure","unsure"),
+      #     respType="hit",fq=fqs_hitRate_perConfidence)
+      #   )
       
       fqs_faRate_perConfidence <- fx(data_x=data_faRate)
-      fqs_faRate_sum <- sum(fqs_faRate_perConfidence,na.rm=T)
-      df_code_i <- rbind(df_code_i,
-        data.frame(code=curCode,qType=qTypeNames[x],targetPosition=2,
-          rating=c("sure","quiteSure","unsure"),
-          respType="fa",fq=fqs_faRate_perConfidence)
-        )
+      fqs_faRate_sum <- sum(fqs_faRate_perConfidence)
+      # df_code_i <- rbind(df_code_i,
+      #   data.frame(code=curCode,qType=qTypeNames[x],targetPosition=2,
+      #     rating=c("sure","quiteSure","unsure"),
+      #     respType="fa",fq=fqs_faRate_perConfidence)
+      #   )
       
       fqs_missRate_perConfidence <- fx(data_x=data_missRate)
-      fqs_missRate_sum <- sum(fqs_missRate_perConfidence,na.rm=T)
-      df_code_i <- rbind(df_code_i,
-        data.frame(code=curCode,qType=qTypeNames[x],targetPosition=1,
-          rating=c("sure","quiteSure","unsure"),
-          respType="miss",fq=fqs_missRate_perConfidence)
-        )
+      fqs_missRate_sum <- sum(fqs_missRate_perConfidence)
+      # df_code_i <- rbind(df_code_i,
+      #   data.frame(code=curCode,qType=qTypeNames[x],targetPosition=1,
+      #     rating=c("sure","quiteSure","unsure"),
+      #     respType="miss",fq=fqs_missRate_perConfidence)
+      #   )
 
       fqs_crRate_perConfidence <- fx(data_x=data_crRate)
-      fqs_crRate_sum <- sum(fqs_crRate_perConfidence,na.rm=T)
-      df_code_i <- rbind(df_code_i,
-        data.frame(code=curCode,qType=qTypeNames[x],targetPosition=2,
-          rating=c("sure","quiteSure","unsure"),
-          respType="cr",fq=fqs_crRate_perConfidence)
-        )
+      fqs_crRate_sum <- sum(fqs_crRate_perConfidence)
+      # df_code_i <- rbind(df_code_i,
+      #   data.frame(code=curCode,qType=qTypeNames[x],targetPosition=2,
+      #     rating=c("sure","quiteSure","unsure"),
+      #     respType="cr",fq=fqs_crRate_perConfidence)
+      #   )
 
       sumOfsums <- sum(c(fqs_hitRate_sum,fqs_faRate_sum,fqs_missRate_sum,fqs_crRate_sum))
       hitRate <- fqs_hitRate_sum/sum(c(fqs_hitRate_sum,fqs_missRate_sum))
+      if(hitRate==1){
+        hitRate <- 1 - 1/(2*N_sample) # https://stats.stackexchange.com/questions/134779/d-prime-with-100-hit-rate-probability-and-0-false-alarm-probability
+      }
       faRate <- fqs_faRate_sum/sum(c(fqs_faRate_sum,fqs_crRate_sum))
+      if(faRate==0){
+        faRate <- 1/(2*N_sample)
+      }
       dPrime <- qnorm(hitRate)-qnorm(faRate)
 
       df2_code_i <- rbind(df2_code_i,
         data.frame(code=curCode,qName=qTypeNames[x],
-          tns=tns,pts=pts,queriedPosition=qPos,accessoryPosition=accPos,
+          tns=tns,pts=pts,queriedPosition=x,accessoryPosition=accPos,
           qType="eq",
           hit_fq=fqs_hitRate_sum,fa_fq=fqs_faRate_sum,
           miss_fq=fqs_missRate_sum,cr_fq=fqs_crRate_sum,
@@ -134,9 +141,23 @@ aggrFx <- function(pts,tns,qPos,accPos){
   }
   return(df)
 }
-same_low_1_1 <- aggrFx(pts="Same",tns="low",qPos=1,accPos=1)
-same_low_1_2 <- aggrFx(pts="Same",tns="low",qPos=1,accPos=2)
-print(same_low_1_1)
+same_low_1_1 <- aggrFx(pts="Same",tns="low",accPos=1)
+same_low_1_2 <- aggrFx(pts="Same",tns="low",accPos=2)
+same_high_1_1 <- aggrFx(pts="Same",tns="high",accPos=1)
+same_high_1_2 <- aggrFx(pts="Same",tns="high",accPos=2)
+
+print(
+  aggregate(dPrime ~ queriedPosition,mean,data=same_low_1_1)
+)
+print(
+  aggregate(dPrime ~ queriedPosition,mean,data=same_low_1_2)
+)
+print(
+  aggregate(dPrime ~ queriedPosition,mean,data=same_high_1_1)
+)
+print(
+  aggregate(dPrime ~ queriedPosition,mean,data=same_high_1_2)
+)
 # aggregate over conditions per participant
 
 
