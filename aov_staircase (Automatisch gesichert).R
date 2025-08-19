fb4ac6a4bc76384d4a4f0c99af1d217c56cea3c3
pseiti <36700524+pseiti@users.noreply.github.com>
2025-08-19 12:12:53 +0200

    staircase

--- aov_staircase (Automatisch gesichert).R
+++ aov_staircase (Automatisch gesichert).R
@@ -7,14 +7,29 @@ A_data <- data[A_rows,]
 B_data <- data[A_rows==FALSE,]
 unique_tracks_A <- unique(A_data[,"track"])
 unique_tracks_B <- unique(B_data[,"track"])
+
 df <- NULL
+nLastElements <- 10
 for(i in 1:length(unique_tracks_A)){
 	i_track <- unique_tracks_A[i]
 	A_track_i_rows <- A_data[,"track"]==i_track
 	nRows <- length(which(A_track_i_rows)==TRUE)
 	A_track_i_CBAs <- A_data[A_track_i_rows,"cur_back_ang"]
+	A_track_i_CBAs <- tail(A_track_i_CBAs,nLastElements)
 	df <- rbind(df,
-		data.frame(A_or_B="A",track=i_track,x=c(1:nRows),CBA=A_track_i_CBAs)
+		data.frame(A_or_B="A",track=i_track,x=c(1:nLastElements),CBA=A_track_i_CBAs)
 	)
 }
 aggregate(CBA~x,mean,data=df)
+
+for(i in 1:length(unique_tracks_B)){
+	i_track <- unique_tracks_B[i]
+	B_track_i_rows <- B_data[,"track"]==i_track
+	nRows <- length(which(B_track_i_rows)==TRUE)
+	B_track_i_CBAs <- B_data[B_track_i_rows,"cur_back_ang"]
+	B_track_i_CBAs <- tail(B_track_i_CBAs,nLastElements)
+	df <- rbind(df,
+		data.frame(A_or_B="B",track=i_track,x=c(1:nLastElements),CBA=B_track_i_CBAs)
+	)
+}
+aggregate(CBA~x*A_or_B,mean,data=df)
--- aov_staircase.R
+++ aov_staircase.R
@@ -3,11 +3,33 @@ getwd()
 setwd("/Users/paulseitlinger/Documents/GitHub/STIWA")
 data <- read.csv("_5f6hb2#.csv")
 A_rows <- data$A_or_B_track=="A"
-unique_tracks_A <- unique(data[A_rows,"track"])
-unique_tracks_B <- unique(data[A_rows==FALSE,"track"])
+A_data <- data[A_rows,]
+B_data <- data[A_rows==FALSE,]
+unique_tracks_A <- unique(A_data[,"track"])
+unique_tracks_B <- unique(B_data[,"track"])
+
 df <- NULL
-for(i in unique_tracks_A){
-	A_track_i_rows <- data[A_rows,"track"]==i
-	A_track_i_CBAs <- data[A_track_i_rows,"cur_back_ang"]
-	df <- rbind(df,data.frame(A_or_B="A",x=1:length(A_track_i_CBAs),CBA= A_track_i_CBAs))
+nLastElements <- 10
+for(i in 1:length(unique_tracks_A)){
+	i_track <- unique_tracks_A[i]
+	A_track_i_rows <- A_data[,"track"]==i_track
+	nRows <- length(which(A_track_i_rows)==TRUE)
+	A_track_i_CBAs <- A_data[A_track_i_rows,"cur_back_ang"]
+	A_track_i_CBAs <- tail(A_track_i_CBAs,nLastElements)
+	df <- rbind(df,
+		data.frame(A_or_B="A",track=i_track,x=c(1:nLastElements),CBA=A_track_i_CBAs)
+	)
 }
+aggregate(CBA~x,mean,data=df)
+
+for(i in 1:length(unique_tracks_B)){
+	i_track <- unique_tracks_B[i]
+	B_track_i_rows <- B_data[,"track"]==i_track
+	nRows <- length(which(B_track_i_rows)==TRUE)
+	B_track_i_CBAs <- B_data[B_track_i_rows,"cur_back_ang"]
+	B_track_i_CBAs <- tail(B_track_i_CBAs,nLastElements)
+	df <- rbind(df,
+		data.frame(A_or_B="A",track=i_track,x=c(1:nLastElements),CBA=A_track_i_CBAs)
+	)
+}
+aggregate(CBA~x,mean,data=df)
