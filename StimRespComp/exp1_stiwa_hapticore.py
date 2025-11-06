bab2fa1550532e9689678b52d6728df48720f55f
pseiti <36700524+pseiti@users.noreply.github.com>
2025-11-06 14:43:42 +0100

    sdf

--- StimRespComp/exp1_stiwa_hapticore.py
+++ StimRespComp/exp1_stiwa_hapticore.py
@@ -10,18 +10,40 @@ import time
 import scipy.stats as st
 from scipy.stats import norm
 
+class helpr_functions:
+
+	def __init__(self, font, fontSize):
+		self.font = font
+		self.fontSize = fontSize
+
+	def open_txtWindow_fx(self, window, title, txt, dimensions):
+		newWindow = Toplevel(window)
+		newWindow.title(title)
+		newWindow.geometry(dimensions)
+		message = Label(newWindow, text = txt)
+		message.config(font = (self.font, self.fontSize))
+		message.pack()
+
+
 class main_functions:
 
 	def __init__(self,phase,stimuli):
 		self.phase = phase
 		self.stimuli = stimuli
 
+
+H = helpr_functions(font = "Arial", fontSize = 16)
 root = Tk()
 root.title("FFG-STIWA, Experiment 1.2")
-root.geometry("400x500+50+250")
+root.geometry("400x400+50+150")
 intro_message = Label(root, text = """Please click on the instructions button
 and read the information carefully!""")
 intro_message.config(font=("Arial",16))
-intro_message.pack()
+intro_message.pack(pady=10)
+instruction_btn = Button(root, text = "Instructions")
+instruction_btn.config(command = lambda: H.open_txtWindow_fx(
+	window = root, title = "Instruction", txt = "...", 
+	dimensions = "600x400+500+50"))
+instruction_btn.pack()
 
 root.mainloop()
